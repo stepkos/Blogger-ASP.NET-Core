41cd3bc0f7da668a5f8df898298f90fe32c18995
jstepkowski <jstepkowski@K40145>
2021-07-07 15:54:29 +0200

    Add project files.

---
+++ Application/Services/PostService.cs
@@ -0,0 +1,43 @@
+﻿using Application.Dto;
+using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public IEnumerable<PostDto> GetAllPosts()
+        {
+            var posts = _postRepository.GetAll();
+            return posts.Select(post => new PostDto { 
+                Id = post.Id,
+                Title = post.Title,
+                Content = post.Content
+            });
+        }
+
+        public PostDto GetPostById(int id)
+        {
+            var post = _postRepository.GetById(id);
+            return new PostDto()
+            {
+                Id = post.Id,
+                Title = post.Title,
+                Content = post.Content
+            };
+        }
+    }
+}
---
+++ Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private static readonly ISet<Post> _posts = new HashSet<Post>()
+        {
+            new Post(1, "Tytul 1", "Tresc 1"),
+            new Post(2, "Tytul 2", "Tresc 2"),
+            new Post(3, "Tytul 3", "Tresc 3")
+        };
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _posts;
+        }
+
+        public Post GetById(int id)
+        {
+            return _posts.SingleOrDefault(x => x.Id == id);
+        }
+        public Post Add(Post post)
+        {
+            post.Id = _posts.Count() + 1;
+            post.Created = DateTime.UtcNow;
+            _posts.Add(post);
+            return post;
+        }
+
+        public void Update(Post post)
+        {
+            post.LastModified = DateTime.UtcNow;
+        }
+
+        public void Delete(Post post)
+        {
+            _posts.Remove(post);
+        }
+    }
+}
