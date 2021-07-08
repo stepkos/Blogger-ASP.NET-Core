78c206fbafe34f6a90fa6f90b903b0151587e7ff
Jakub Stępkowski <jstepkowski@neuca.pl>
2021-07-08 09:48:56 +0200

    AutoMapper - auto map scope from Post to PostDto

--- Application/Services/PostService.cs
+++ Application/Services/PostService.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Dto;
 using Application.Interfaces;
+using AutoMapper;
 using Domain.Entities;
 using Domain.Interfaces;
 using System;
@@ -13,31 +14,24 @@ namespace Application.Services
     public class PostService : IPostService
     {
         private readonly IPostRepository _postRepository;
+        private readonly IMapper _mapper;
 
-        public PostService(IPostRepository postRepository)
+        public PostService(IPostRepository postRepository, IMapper mapper)
         {
             _postRepository = postRepository;
+            _mapper = mapper;
         }
 
         public IEnumerable<PostDto> GetAllPosts()
         {
             var posts = _postRepository.GetAll();
-            return posts.Select(post => new PostDto { 
-                Id = post.Id,
-                Title = post.Title,
-                Content = post.Content
-            });
+            return _mapper.Map<IEnumerable<PostDto>>(posts);
         }
 
         public PostDto GetPostById(int id)
         {
             var post = _postRepository.GetById(id);
-            return new PostDto()
-            {
-                Id = post.Id,
-                Title = post.Title,
-                Content = post.Content
-            };
+            return _mapper.Map<PostDto>(post);
         }
     }
 }
