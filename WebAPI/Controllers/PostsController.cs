7f51b411abee6076a62b1aa0bbdd80354bcc7a37
Jakub Stępkowski <jstepkowski@neuca.pl>
2021-07-08 12:03:53 +0200

    PostsController

---
+++ WebAPI/Controllers/PostsController.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public PostsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var posts = _postService.GetAllPosts();
+            return Ok(posts);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postService.GetPostById(id);
+            if (post == null)
+                return NotFound();
+            return Ok(post);
+        }
+
+
+    }
+}
